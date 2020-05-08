4fe6a22dea1edde3cc92346e4e7b89773f9a211a
annbgn <fake@mail.com>
2020-05-08 20:30:44 +0300

    add dfmt formatter and gitignore

--- server/source/app.d
+++ server/source/app.d
@@ -7,157 +7,164 @@ import core.stdc.stdlib;
 import vibe.d;
 import std.datetime.timezone : LocalTime;
 
-class Game { 
-	const uint rows = 15;
-	const uint cols = 15;
-	char[rows][cols] field;
-	char current = 'X'; 
-	char server_mark;
-	char client_mark;
-	Direction[] allDirections = [Direction(1,0), Direction(0,1), Direction(1,1), Direction(1,-1)];
+class Game {
+    const uint rows = 15;
+    const uint cols = 15;
+    char[rows][cols] field;
+    char current = 'X';
+    char server_mark;
+    char client_mark;
+    Direction[] allDirections = [
+        Direction(1, 0), Direction(0, 1), Direction(1, 1), Direction(1, -1)
+    ];
 
-	
-	void render () @safe {
-		write("  ");
-		char letter = 'A';
-		for(int i = 0; i < cols; i++){
-			write("|");
-			write(letter);
-			letter++;
-		}
-		write("|\n");
+    void render() @safe {
+        write("  ");
+        char letter = 'A';
+        for (int i = 0; i < cols; i++) {
+            write("|");
+            write(letter);
+            letter++;
+        }
+        write("|\n");
 
-		char letter2 = 'a';
-		for(int i = 0; i<rows; i++) {
-			write("  ");
-			for(int j = 0; j<cols;j++){
-				write("+-");
-			}
-			write("+\n ");
-			write(letter2);
-			for(int j = 0; j<cols;j++){
-				write("|");
-				if(field[i][j]!= 'X' && field[i][j]!='O')
-					write(" ");
-				else
-					write(field[i][j]);
-			}
-			write("|\n");
-			letter2++;
-			}
-		write("  ");
-		for(int j = 0; j<cols;j++){
-				write("+-");
-			}
-			write("+\n");
-	}
+        char letter2 = 'a';
+        for (int i = 0; i < rows; i++) {
+            write("  ");
+            for (int j = 0; j < cols; j++) {
+                write("+-");
+            }
+            write("+\n ");
+            write(letter2);
+            for (int j = 0; j < cols; j++) {
+                write("|");
+                if (field[i][j] != 'X' && field[i][j] != 'O')
+                    write(" ");
+                else
+                    write(field[i][j]);
+            }
+            write("|\n");
+            letter2++;
+        }
+        write("  ");
+        for (int j = 0; j < cols; j++) {
+            write("+-");
+        }
+        write("+\n");
+    }
 
-	void changeCurrent() @safe{current = reverse_mark(current);}
+    void changeCurrent() @safe {
+        current = reverse_mark(current);
+    }
 
+    auto cellsAround(Position pos, Direction d) {
+        // d - is an element of  alldirections so it might be Direction(1,0), Direction(0,1), Direction(1,1), Direction(1,-1)
+        auto helperfunc = (Position p) {
+            if ((p.i < rows) && (p.j < cols))
+                return field[p.i][p.j];
+            return '\0';
+        }; //allows not to get rangeerror and not affect hassequence logic
+        auto res = around(pos, d, 4).map!(p => helperfunc(p));
+        writeln(res);
+        return res;
+    }
 
-	auto cellsAround (Position pos, Direction d) {
-	// d - is an element of  alldirections so it might be Direction(1,0), Direction(0,1), Direction(1,1), Direction(1,-1)
-	auto helperfunc = (Position p) {if ((p.i < rows) &&(p.j < cols)) return field[p.i][p.j]; return '\0';}; //allows not to get rangeerror and not affect hassequence logic
-	auto res = around(pos, d, 4).map!(p=> helperfunc(p)); 
-	writeln(res);
-	return res;
-  }
+    bool gameOver(Position pos, char mark) {
+        bool ended = allDirections.any!(d => cellsAround(pos, d).hasSequence(mark, 5)); //point pos(i,j) in any of direcrions of alldirections has 5X sequence ( BUT CAN IT BE 0????)
+        bool draw = is_draw();
+        return (ended || draw);
+    }
 
-	bool gameOver(Position pos, char mark){
-		bool ended = 
-			allDirections.any!(d => cellsAround(pos, d).hasSequence(mark, 5)); //point pos(i,j) in any of direcrions of alldirections has 5X sequence ( BUT CAN IT BE 0????)
-		bool draw = is_draw();
-		return (ended||draw);
-	}
+    bool is_draw() {
+        return false;
+    }
 
-	bool is_draw (){return false;}
+    void setInput(Position pos) @safe {
+        field[pos.i][pos.j] = current;
+    }
 
-	void setInput(Position pos)  @safe {
-		field[pos.i][pos.j] = current;
-	}
-	
 }
-bool hasSequence(Range, V) (Range r, V val, size_t target) {
-	size_t counter = 0;
-	foreach(e; r){
-		if(e == val){
-			counter++;
-			// cool place to embed ai logic
-			if(counter == target)
-				return true;
-		}
-		else
-			counter = 0;
-	}
-	return false;
+
+bool hasSequence(Range, V)(Range r, V val, size_t target) {
+    size_t counter = 0;
+    foreach (e; r) {
+        if (e == val) {
+            counter++;
+            // cool place to embed ai logic
+            if (counter == target)
+                return true;
+        }
+        else
+            counter = 0;
+    }
+    return false;
 }
 
 struct PosToDirRange {
-	Position front;
-	Position end;
-	Direction dir;
+    Position front;
+    Position end;
+    Direction dir;
 
-	bool empty () {
-		return front == end;
-	}
+    bool empty() {
+        return front == end;
+    }
 
-	void popFront() {
-		front.i += dir.i;
-		front.j += dir.j;
-	}
+    void popFront() {
+        front.i += dir.i;
+        front.j += dir.j;
+    }
 }
 
 PosToDirRange around(Position center, Direction dir, uint radius) {
-	// returns radius(4) nearest points to center(i,j) in chosen direction
-	// if near border, then less than 4
-	uint left = min(radius, borderDistance(center, minusDir(dir)));
-	uint right = min(radius, borderDistance(center, dir)) + 1;
-	auto res = PosToDirRange(
-		Position(center.i - dir.i*left, center.j - dir.j*left),
-		Position(center.i + dir.i*right, center.j + dir.j*right),
-		dir
-	);
-	writeln(res);
-	return res;
+    // returns radius(4) nearest points to center(i,j) in chosen direction
+    // if near border, then less than 4
+    uint left = min(radius, borderDistance(center, minusDir(dir)));
+    uint right = min(radius, borderDistance(center, dir)) + 1;
+    auto res = PosToDirRange(Position(center.i - dir.i * left,
+            center.j - dir.j * left), Position(center.i + dir.i * right,
+            center.j + dir.j * right), dir);
+    writeln(res);
+    return res;
 }
-
-
-
 
 uint borderDistance(uint p, int d) {
-	// 14 is field rows(=cols) -1
-	if(d<0) return p;
-	else if( d== 0) return 14;
-	else return 14 -p;
+    // 14 is field rows(=cols) -1
+    if (d < 0)
+        return p;
+    else if (d == 0)
+        return 14;
+    else
+        return 14 - p;
 }
 
-uint borderDistance(Position pos, Direction dir){
-	return min(borderDistance(pos.i, dir.i),
-			borderDistance(pos.j, dir.j));
+uint borderDistance(Position pos, Direction dir) {
+    return min(borderDistance(pos.i, dir.i), borderDistance(pos.j, dir.j));
 }
 
 struct Position {
-	uint i;
-	uint j;
-}
-struct Direction {
-	uint i,j;
-		
-}
-Direction minusDir(Direction d) {
-	Direction dir;
-	dir.i=-d.i;
-	dir.j=-d.j;
-	return dir;
+    uint i;
+    uint j;
 }
 
-Position readInput(string s){
-	int x = s[0] - 'a';
-	int y = s[1] - 'A';
-	Position input;
-	input.i = x;
-	input.j = y;
-	return input;
+struct Direction {
+    uint i, j;
+
+}
+
+Direction minusDir(Direction d) {
+    Direction dir;
+    dir.i = -d.i;
+    dir.j = -d.j;
+    return dir;
+}
+
+Position readInput(string s) {
+    int x = s[0] - 'a';
+    int y = s[1] - 'A';
+    Position input;
+    input.i = x;
+    input.j = y;
+    return input;
 }
 
 /*
@@ -182,58 +189,73 @@ class ConsoleStream : IControlStream {
 	TCPConnection conn;
 }
 */
-char reverse_mark(char mark) @safe { if (mark == 'X') return 'O'; return 'X';}
+char reverse_mark(char mark) @safe {
+    if (mark == 'X')
+        return 'O';
+    return 'X';
+}
 
-void main (	) @trusted {
-	listenTCP(7000,(conn) {
-		Game game = new Game;
-		game.render();
-		string inputString = "";
-		bool gameOver = false;
-		Position inputPosition;
+void main() @trusted {
+    listenTCP(7000, (conn) {
+        Game game = new Game;
+        game.render();
+        string inputString = "";
+        bool gameOver = false;
+        Position inputPosition;
 
-		// send client it's mark
-		SysTime today = Clock.currTime();
-		if (today.dayOfYear % 2 == 0) { conn.write("O\r\n"); game.server_mark = 'X';}
-		else {conn.write("X\r\n"); game.server_mark = 'O';}
-		game.client_mark = reverse_mark(game.server_mark);
+        // send client it's mark
+        SysTime today = Clock.currTime();
+        if (today.dayOfYear % 2 == 0) {
+            conn.write("O\r\n");
+            game.server_mark = 'X';
+        }
+        else {
+            conn.write("X\r\n");
+            game.server_mark = 'O';
+        }
+        game.client_mark = reverse_mark(game.server_mark);
 
-		writeln("client: ", game.client_mark, " server: ", game.server_mark);
+        writeln("client: ", game.client_mark, " server: ", game.server_mark);
 
-		while(!gameOver) {
+        while (!gameOver) {
 
-			writeln();
-			if(game.current == game.server_mark) {
+            writeln();
+            if (game.current == game.server_mark) {
 
-				while(1) {
-					write("Hi, Server (", game.server_mark, ")! hint: type aA: ");
-					inputString = readln();
-					inputPosition = readInput(inputString); 
-					if((inputString.length != 2+1 )|| (inputPosition.i > game.rows ) || (inputPosition.j > game.cols)||(game.field[inputPosition.i][inputPosition.j]==game.client_mark)|| (game.field[inputPosition.i][inputPosition.j]==game.server_mark))
-						write("bad move!\n");
-					else break;
-				}
-				game.setInput(inputPosition);
-				inputString = inputString ~ "\r\n";
-				conn.write(inputString);
-				gameOver = game.gameOver(inputPosition, game.server_mark);
-			}
-			else {
-				write("Waiting for Client (" , game.client_mark, ")'s turn..\n");
-				string opponentInputString = cast(string)conn.readLine();
-				writeln("received ", opponentInputString);
-				auto opponentInputPosition = readInput(opponentInputString);
-				game.setInput(opponentInputPosition);
-				gameOver = game.gameOver(opponentInputPosition, game.client_mark);
-				
-			}
-			if(!gameOver) game.changeCurrent();
-				//system("cls");
-				game.render();
-		}
-		writeln("Congratulations, ", game.current, " !");
-	});
-	runApplication();
+                while (1) {
+                    write("Hi, Server (", game.server_mark, ")! hint: type aA: ");
+                    inputString = readln();
+                    inputPosition = readInput(inputString);
+                    if ((inputString.length != 2 + 1) || (inputPosition.i > game.rows)
+                        || (inputPosition.j > game.cols)
+                        || (game.field[inputPosition.i][inputPosition.j] == game.client_mark)
+                        || (game.field[inputPosition.i][inputPosition.j] == game.server_mark))
+                        write("bad move!\n");
+                    else
+                        break;
+                }
+                game.setInput(inputPosition);
+                inputString = inputString ~ "\r\n";
+                conn.write(inputString);
+                gameOver = game.gameOver(inputPosition, game.server_mark);
+            }
+            else {
+                write("Waiting for Client (", game.client_mark, ")'s turn..\n");
+                string opponentInputString = cast(string) conn.readLine();
+                writeln("received ", opponentInputString);
+                auto opponentInputPosition = readInput(opponentInputString);
+                game.setInput(opponentInputPosition);
+                gameOver = game.gameOver(opponentInputPosition, game.client_mark);
+
+            }
+            if (!gameOver)
+                game.changeCurrent();
+            //system("cls");
+            game.render();
+        }
+        writeln("Congratulations, ", game.current, " !");
+    });
+    runApplication();
 }
 /*
 	unittest {
