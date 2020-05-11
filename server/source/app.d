f5fcf2f5245935c9313bfd40940d01aecc899432
annbgn <fake@mail.com>
2020-05-11 11:11:41 +0300

    WIP tree AI new in branch: - tree structure - add avaliable positions - sort availiables by distance to center - estimate state based on regex matches (only non-overlapping matches) - how to upgrade: chose position which increases amount of matches - amazing amount of children in trees - loops are limited (inperfect, redo)

--- server/source/app.d
+++ server/source/app.d
@@ -8,6 +8,7 @@ import vibe.d;
 import std.datetime.timezone : LocalTime;
 import std.regex;
 import std.array : array;
+import std.array : replicate;
 import std.algorithm : canFind;
 
 struct EstimationElem {
@@ -20,33 +21,60 @@ const EstimationElem[] GlobalEstimationChart = [
     EstimationElem(10000, "*****"),
 	EstimationElem(1000, " **** "),
     EstimationElem(500, "**** "),
+	EstimationElem(500, " ****"),
 	EstimationElem(400, "* ***"),
+	EstimationElem(400, "*** *"),
 	EstimationElem(400, "** **"),
-    EstimationElem(100, "  ***   "),
-	EstimationElem(80, "  ***  "),
-    EstimationElem(75, " ***  "),
+    EstimationElem(100, "  ***  "),
+	EstimationElem(80, "  *** "),
+    EstimationElem(80, " ***  "),
 	EstimationElem(50, " *** "),
 	EstimationElem(50, "***  "),
+	EstimationElem(50, "  ***"),
     EstimationElem(25, "* ** "),
+	EstimationElem(25, " * **"),
 	EstimationElem(25, "** * "),
+	EstimationElem(25, " ** *"),
 	EstimationElem(25,  "*  **"),
-    EstimationElem(10, "   ***   "),
-	EstimationElem(5, " ** ")
+	EstimationElem(25,  "**  *"),
+	EstimationElem(25,  "* * *"),
+	EstimationElem(5, " **  "),
+	EstimationElem(5, "  ** "),
+	EstimationElem(5, " * * "),
+	EstimationElem(5, "**   "),
+	EstimationElem(5, "   **"),
+	EstimationElem(1, "*    "),
+	EstimationElem(1, " *   "),
+	EstimationElem(1, "  *  "),
+	EstimationElem(1, "   * "),
+	EstimationElem(1, "    *")
 ];
 //dfmt on
 
 const string GlobalEmptyPattern = "    *    ";
 
+class Tree {
+    char[15][15] fld;
+    int estimation = 0;
+    Position chosen_move;
+    Tree[] children = [];
+    Tree root;
+}
+
+char[15][15]  init_field() {char[15][15] fld; for(int i = 0; i < 15;i++){for (int j = 0; j < 15; j++)fld[i][j] = ' ';} return fld;}
+
 class Game {
     const uint rows = 15;
     const uint cols = 15;
-    char[rows][cols] field;
+    char[rows][cols] field = init_field();
     char current = 'X';
     char server_mark;
     char client_mark;
     Direction[] allDirections = [
         Direction(1, 0), Direction(0, 1), Direction(1, 1), Direction(1, -1)
     ];
+
+	
 
     void render() @safe {
         write("  ");
@@ -68,7 +96,7 @@ class Game {
             write(letter2);
             for (int j = 0; j < cols; j++) {
                 write("|");
-                if (field[i][j] != 'X' && field[i][j] != 'O')
+                if ((field[i][j] != 'X') && (field[i][j] != 'O'))
                     write(" ");
                 else
                     write(field[i][j]);
@@ -87,22 +115,27 @@ class Game {
         current = reverse_mark(current);
     }
 
-    string cellsAround(Position pos, Direction d) {
+    string cellsAround(Position pos, Direction d, char[rows][cols] fld) {
         // d - is an element of  alldirections so it might be Direction(1,0), Direction(0,1), Direction(1,1), Direction(1,-1)
         auto helperfunc = (Position p) {
             if ((p.i < rows) && (p.j < cols))
-                return field[p.i][p.j];
+                return fld[p.i][p.j];
             return '\0';
         }; //allows not to get rangeerror and not affect hassequence logic
         auto res = around(pos, d, 4).map!(p => helperfunc(p));
-        writeln(res);
         return to!string(res.array); // mapResult -> char[] -> string
     }
 
     bool gameOver(Position pos, char mark) {
-        bool ended = allDirections.any!(d => cellsAround(pos, d).hasSequence(mark, 5)); //point pos(i,j) in any of direcrions of alldirections has 5X sequence ( BUT CAN IT BE 0????)
+        bool ended = allDirections.any!(d => cellsAround(pos, d, field).hasSequence(mark, 5)); //point pos(i,j) in any of direcrions of alldirections has 5X sequence ( BUT CAN IT BE 0????)
         bool draw = is_draw();
         return (ended || draw);
+    }
+
+	bool is_skip(Position pos, char[rows][cols] fld) {
+		auto helperfunc = (Direction d) {auto str = (cellsAround(pos, d, fld)); return (str == to!string(replicate(" ", str.length)));}; // bc isWhite works only with char, not string		
+        bool empty = allDirections.all!(helperfunc);
+        return empty;
     }
 
     bool is_draw() {
@@ -121,61 +154,183 @@ class Game {
         field[pos.i][pos.j] = current;
     }
 
-    GoodPositionToMove[] getGoodPositionsToMove(Position[] possible_moves, int depth) { //maximin
-        // not implemented!!
-        // get all empty cells
-        // get array of weight for every cell 
-        // recursively call this function <depth times> for player and opponent for every cell
-        // 
-        Position p;
-        GoodPositionToMove[] result = [];
-        int[rows][cols] cell_weights;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++)
-                cell_weights[i][j] = 0;
-        }
+    int estimate_state(char player_mark, char[rows][cols] fld) {
+        //counts matches for evety point for every regex pattern
+        //i don't even wanna think what nesting depth is. it'd been a total mistake to implement trees
+        int result = 0;
+        string pattern;
+		string line;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-
                 foreach (Direction d; allDirections) {
-                    string line = cellsAround(Position(i, j), d);
+                    line = cellsAround(Position(i, j), d, fld);
                     foreach (EstimationElem weight_regex; GlobalEstimationChart) {
-                        string pattern = replace(weight_regex.pattern, '*', current);
+                        pattern = replace(weight_regex.pattern, '*', player_mark);
                         if (!matchAll(line, pattern).empty) {
-                            cell_weights[i][j] = cell_weights[i][j] + weight_regex.weight;
+                            result += weight_regex.weight;
+                        }
+						pattern = replace(weight_regex.pattern, '*', reverse_mark(player_mark));
+                        if (!matchAll(line, pattern).empty) {
+                            result -= weight_regex.weight;
                         }
                     }
-
                 }
-                p.i = i;
-                p.j = j;
-                if (possible_moves.canFind(p))
-                    result ~= [GoodPositionToMove(p, cell_weights[i][j])];
             }
-
         }
         return result;
     }
-
-    Position where_to_move(int depth) {
-        Position[] possible_moves = [];
-
-        //todo replace loop with any! or map! for short
+	
+    Position[] get_empty_positions(char[rows][cols] fld) {
+		
+        Position[] res = [];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if ((field[i][j] != 'X') && (field[i][j] != 'O'))
-                    possible_moves ~= [Position(i, j)];
+                if (fld[i][j] == ' ')
+                    res ~= [Position(i,j)];
             }
         }
-        if (possible_moves.length == rows * cols)
-            return Position(to!int(rows / 2), to!int(cols / 2));
-        else {
-            GoodPositionToMove[] good = getGoodPositionsToMove(possible_moves, depth);
-            writeln(good);
-            return good.sort!("a.weight > b.weight")[0].pos;
-        }
-    }
+		// sort to start as close to the center as possible, but it works weirdly.
+		// i expect smth like (7,7), (6,7), (7,6), (7,8), (8,7), (6,6) ...
+		// but i get Position(0, 14), Position(1, 13), Position(14, 0), Position(2, 12), Position(13, 1), Position(3, 11), Position(12, 2), Position(4, 10)
+		// solved: it was magic of uint
+		// todo: rewrite Position(uint i, uint j) to ints  and remove to!int below
+        return res.sort!("(abs(7-to!int(a.i)) + abs(7-to!int(a.j))) < (abs(7-to!int(b.i)) + abs(7-to!int(b.j)))").array;  
+    }    
 
+    Position where_to_move(int depth, int width) {
+		// no matter what depth is
+		// but width is what  matters. it limits availiable and empty cells by <width> closest to center	
+        auto root = new Tree;
+        root.fld = field;
+        auto possibilities = get_empty_positions(field);
+		writeln(possibilities);
+		auto tmp = new Tree;
+
+		if (possibilities.length == rows*cols) return Position(to!int(rows /2), to!int(cols/2));
+
+        // let depth = 4, bc i REALLY  can't imagine how to write a loop which can generate possibilities.length nodes on 1 step, possibilities.length - i nodes on i-th step, where i stands for depth. And how to fill in this structure after it is initialized :(
+        // i guess it might work if d had smth like __getattr__('children') for lvl in range(depth)
+
+		// todo rewrite it in evals :smartass: :smiling_imp:
+		// :sad_imp: it seems there's no eval() func in D except arith-eval lib, but this one supports only math operaors
+
+        foreach (Position pos; possibilities[0..min(possibilities.length-1, width)]) {
+            if (is_skip(pos, field)) continue;
+			tmp = new Tree;
+			tmp.fld = field;				
+			tmp.root = root;
+            tmp.chosen_move = pos;
+            tmp.fld[pos.i][pos.j] = server_mark;
+            root.children ~= [tmp];
+        }
+		writeln ("here ", root.children.length); // 1 - 12-32, 2 < 64
+		int cntr = 0;
+		int cntr2 = 0;
+        foreach (Tree child; root.children) {
+			auto empties =  get_empty_positions(child.fld);
+            foreach (Position pos; empties[0..min(empties.length-1, to!int(width*0.75))]) {
+				cntr ++;
+                if (is_skip(pos, child.fld)) continue;
+				cntr2++;
+				tmp = new Tree;
+				tmp.fld = child.fld;
+                tmp.root = child;
+                tmp.chosen_move = pos;
+                tmp.fld[pos.i][pos.j] = client_mark;
+				tmp.estimation = estimate_state(server_mark, tmp.fld);
+                child.children ~= [tmp];
+            }
+        }
+		writeln ("here2 ",  cntr, " ", cntr2);  // 20 * 15  
+		/*
+        foreach (Tree child1; root.children) {
+            foreach (Tree child2; child1.children) {
+                foreach (Position pos; get_empty_positions(child2.fld)) {
+                    if (is_skip(pos, child2.fld)) continue;
+					tmp = new Tree;
+                    tmp.fld = child2.fld;
+					tmp.root = child2;
+                    tmp.chosen_move = pos;
+                    tmp.fld[pos.i][pos.j] = server_mark;
+					//tmp.estimation = estimate_state(server_mark, tmp.fld);
+                    child2.children ~= [tmp];
+                }
+            }
+        }
+		writeln ("here3");
+		
+        foreach (Tree child1; root.children) {
+            foreach (Tree child2; child1.children) {
+                foreach (Tree child3; child2.children) {
+                    foreach (Position pos; get_empty_positions(child3.fld)) {
+                        if (is_skip(pos, child3.fld)) continue;
+						tmp = new Tree;
+                        tmp.fld = child3.fld;
+                        tmp.root = child3;
+                        tmp.chosen_move = pos;
+                        tmp.fld[pos.i][pos.j] = client_mark;
+                        child3.children ~= [tmp];
+                    }
+                }
+            }
+        }
+		writeln ("here4");
+        foreach (Tree child1; root.children) {
+            foreach (Tree child2; child1.children) {
+                foreach (Tree child3; child2.children) {
+                    foreach (Tree child4; child3.children) {
+                        foreach (Position pos; get_empty_positions(child4.fld)) {
+                            if (is_skip(pos, child4.fld)) continue;
+							tmp = new Tree;
+                            tmp.fld = child4.fld;
+                            tmp.root = child4;
+                            tmp.chosen_move = pos;
+                            tmp.fld[pos.i][pos.j] = server_mark;
+                            tmp.estimation = estimate_state(server_mark, tmp.fld);
+                            child4.children ~= [tmp];
+                        }
+                    }
+                }
+            }
+        }
+		*/
+        // and now reduce :)
+
+        auto helpermax = (Tree t1, Tree t2) {
+            if (t1.estimation > t2.estimation)
+                return t1;
+            return t2;
+        };
+        auto helpermin = (Tree t1, Tree t2) {
+            if (t1.estimation < t2.estimation)
+                return t1;
+            return t2;
+        };
+		/*
+		writeln ("here5");
+        foreach (Tree child1; root.children) {
+            foreach (Tree child2; child1.children) {
+                foreach (Tree child3; child2.children) {
+                    child3.estimation = child3.children.fold!(helpermax).estimation;
+                    child3.children = []; // no strong refs for children, hope gc works well
+                }
+            }
+        }
+		writeln ("here6");
+        foreach (Tree child1; root.children) {
+            foreach (Tree child2; child1.children) {
+                child2.estimation = child2.children.fold!(helpermin).estimation;
+                child2.children = [];
+            }
+        }*/
+		writeln ("here7");
+        foreach (Tree child1; root.children) {
+            child1.estimation = child1.children.fold!(helpermax).estimation;
+            child1.children = [];
+        }
+
+        return root.children.fold!(helpermin).chosen_move;
+    }
 }
 
 bool hasSequence(Range, V)(Range r, V val, size_t target) {
@@ -216,7 +371,6 @@ PosToDirRange around(Position center, Direction dir, uint radius) {
     auto res = PosToDirRange(Position(center.i - dir.i * left,
             center.j - dir.j * left), Position(center.i + dir.i * right,
             center.j + dir.j * right), dir);
-    writeln(res);
     return res;
 }
 
@@ -269,50 +423,15 @@ string writeInput(Position pos) {
     return s;
 }
 
-/*
-interface IControlStream {
-	Position read();
-	void write(Position); 
-}
-class ConsoleStream : IControlStream {
-	Position read() {
-		string s = readln(); //aA
-		int x = s[0] - 'a';
-		int y = s[1] - 'A';
-		Position input;
-		input.i = x;
-		input.j = y;
-		return input;
-	}
-	void write(string input) {
-		writeln("Your turn was ", input[0], ", ", input[1]);
-		conn->write(input);
-	}
-	TCPConnection conn;
-}
-*/
 char reverse_mark(char mark) @safe {
     if (mark == 'X')
         return 'O';
     return 'X';
 }
-/*
-int MINIMAX(char[] field, char current_mark, deph) : int
-    if isTerminal(field) then
-        return -heuristic(field, player)
-    end
-    score  = INFINITY
-    for child from children(field, player) do
-        s = MINIMAX(child, -player, deph+1)
-        if s < score  then score  = s
-    end
-    return score
-end
-*/
 
 struct GoodPositionToMove {
     Position pos;
-    int weight; // = int.min;
+    int weight = int.min;
 }
 
 void main() @trusted {
@@ -359,7 +478,7 @@ void main() @trusted {
                 conn.write(inputString);
                 gameOver = game.gameOver(inputPosition, game.server_mark);
 				*/
-                Position move = game.where_to_move(1);
+                Position move = game.where_to_move(4, 20);
                 writeln("Hi, Server (", game.server_mark,
                     ")! AI chose to move to position ", move);
                 game.setInput(move);
@@ -386,13 +505,37 @@ void main() @trusted {
     });
     runApplication();
 }
-/*
-	unittest {
-	// test case when any input with letter bigger than k causes range error
-		char[14][14] field;
-		Position pos;
-		pos  = readInput("kK\0");
 
 
+unittest {
+char[15][15] fld = init_field();
+fld[0][0] = 'X';
+auto game =  new Game;
+
+assert (15 == game.estimate_state('X', fld));
 }
-*/
+
+
+
+unittest {
+char[15][15] fld = init_field();
+fld[0][0] = 'X';
+fld[14][14] = 'O';
+auto game =  new Game;
+
+assert (0 == game.estimate_state('X', fld));
+}
+
+unittest {
+char[15][15] fld = init_field();
+fld[0][0] = 'X';
+int counter = 0;
+
+auto game = new Game;
+for (int i = 0;  i< 15; i++){
+for (int j = 0; j < 15; j++){
+if ((i==0) && (j==0)) continue;
+counter = counter +to!int(game.is_skip(Position(i,j), fld));}}
+writeln (counter);
+assert (counter == 3*4);
+}
